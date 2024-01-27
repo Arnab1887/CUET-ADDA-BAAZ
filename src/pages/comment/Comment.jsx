d9247ba511e724acb60a04e218557b5965c6bc18
Arnab Chakraborty 1804087 <156361365+Arnab1887@users.noreply.github.com>
2024-01-27 17:46:35 +0600

    Add files via upload

---
+++ src/pages/comment/Comment.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import Comments from "../../components/comments/Comments";
+import axios from "axios";
+import { useLocation } from "react-router";
+
+export default function Comment() {
+  const [comments, setComments] = useState([]);
+  const location = useLocation();
+  const path = location.pathname.split("/")[2];
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      const res = await axios.get("/comments/"); //+ path;
+      setComments(res.data);
+    };
+    fetchComments();
+  }, [path]);
+  return (
+    <>
+      <div>
+        <Comments comments={comments} />
+        {/* <Sidebar /> */}
+      </div>
+    </>
+  );
+}
