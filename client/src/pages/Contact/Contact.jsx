485e6dcaecb2d99a0431cd7e016e528109c00228
Arnab Chakraborty 1804087 <156361365+Arnab1887@users.noreply.github.com>
2024-03-20 19:55:32 +0600

    Add files via upload

---
+++ client/src/pages/Contact/Contact.jsx
@@ -0,0 +1,61 @@
+import "./contact.css";
+
+export default function Contact() {
+  return (
+    <div className="singlePost">
+      <div className="singlePostWrapper">
+        <img
+          className="singlePostImg"
+          src="https://scontent.fcgp13-1.fna.fbcdn.net/v/t39.30808-6/375804357_337068742007406_7298823397837294332_n.jpg?stp=dst-jpg_s960x960&_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=PpY6TDeyROAAX99rKH0&_nc_ht=scontent.fcgp13-1.fna&oh=00_AfBwm29TK01ZxfxooUtgTh7y_iDnSe7aZ3pCAzgQT7osXw&oe=65FE29D1"
+          alt=""
+        />
+        <h1 className="singlePostTitle">
+          CUET-ADDA-BAAZ: A platform to share your ideas
+          {/* <div className="singlePostEdit">
+            <i className="singlePostIcon far fa-edit"></i>
+            <i className="singlePostIcon fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-facebook"></i>
+          </div> */}
+        </h1>
+        {/* <div className="singlePostInfo">
+          <span>
+            Author:
+            <b className="singlePostAuthor">Safak</b>
+          </span>
+          <span>1 day ago</span>
+        </div> */}
+        <p className="singlePostDesc">
+          CUET-ADDA-BAAZ is a blogging web app that let users create posts. Users can share their ideas through blog posts here.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
+          quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
+          Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
+          eos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
+          error quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
+          impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas a
+          odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+          a odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+          a odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+          a odit modi eos!
+          <br />
+          <br />
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
+          quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
+          Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
+          eos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
+          error quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
+          impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas a
+          odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+          a odit modi eos! Lorem, ipsum dolor sit amet consectetur.
+        </p>
+      </div>
+    </div>
+  );
+}
